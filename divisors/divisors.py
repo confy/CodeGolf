7a222eb20de617483457845b7bbb1b1a64169802
confy <confuzedfx@gmail.com>
2020-10-24 16:15:47 -0700

    First Commit - Divisors

---
+++ divisors/divisors.py
@@ -0,0 +1,9 @@
+def main():
+    for num in range(1, 101):
+        for i in range(1, num + 1):
+            if num % i == 0:
+                print(i, end=' ')
+        print()
+
+
+main()
---
+++ divisors/divisorsMINI.py
@@ -0,0 +1,6 @@
+D=print
+C=range
+for A in C(1,101):
+	for B in C(1,A+1):
+		if A%B==0:D(B,end=' ')
+	D()
