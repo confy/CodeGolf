61cf9483e9e220add4b6fa1daf78150adfa9f1e5
confy <confuzedfx@gmail.com>
2020-10-24 16:57:45 -0700

    Format Changes

--- divisors/divisorsMINI.py
+++ divisors/divisorsMINI.py
@@ -1,6 +1,7 @@
-D=print
-C=range
-for A in C(1,101):
-	for B in C(1,A+1):
-		if A%B==0:D(B,end=' ')
-	D()
+D = print
+C = range
+for A in C(1, 101):
+    for B in C(1, A+1):
+        if A % B == 0:
+            D(B, end=' ')
+    D()
--- leapyears/leapMINI.py
+++ leapyears/leapMINI.py
@@ -1,2 +1,3 @@
-for A in range(1801,2401):
-	if A%4==0 and(A%400==0 or A%100!=0):print(A)
+for A in range(1801, 2401):
+    if A % 4 == 0 and (A % 400 == 0 or A % 100 != 0):
+        print(A)
