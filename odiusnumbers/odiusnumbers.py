d6dcfce82d30135d077ab8dfe75cd786df260be9
confy <confuzedfx@gmail.com>
2020-10-24 17:14:31 -0700

    Added Evil and Odius numbers

---
+++ odiusnumbers/odiusnumbers.py
@@ -0,0 +1,5 @@
+for i in range(51):
+    binary_rep = str(bin(i))[2:]
+    one_count = binary_rep.count('1')
+    if one_count % 2 != 0:
+        print(i)
---
+++ odiusnumbers/odiusnumbersMINI.py
@@ -0,0 +1,3 @@
+for A in range(51):
+	B=str(bin(A))[2:];C=B.count('1')
+	if C%2!=0:print(A)
